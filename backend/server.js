00a35f02f1d5e32a972f6eb0820552e65306fc57
gaara12-cpu <esparzahernandezv9@gmail.com>
2026-05-22 16:03:57 -0600

    corrige cors render

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const authRutas = require("./routes/authRutas");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://sistema-entregas-three.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(express.json());
 
