d8d5df18d2aef249e4f34de11a65873593cfcf94
gaara12-cpu <esparzahernandezv9@gmail.com>
2026-05-22 15:49:46 -0600

    backend render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sistema-entregas-production.up.railway.app/api",
+  baseURL: "https://sistema-entregas-8g18.onrender.com/api",
 });
 
 export default api;
